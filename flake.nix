5d5e65317cd4181a9ab9bbff37afe861478206c1
stefan.genie <stefanf1337@gmail.com>
2026-07-03 01:18:48 +0300

    add helm to nix flake

--- flake.nix
+++ flake.nix
@@ -15,6 +15,7 @@
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
             k3d
+            helm
             kubectl
             kubernetes-helm
             fluxcd
